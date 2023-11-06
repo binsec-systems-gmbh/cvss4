86fa9b752a0ad327f36e11326646f1f0d62b0a22
Florian Zavatzki <fz@binsec.com>
2023-11-06 09:55:05 +0100

    [Package] add debian directory
    Signed-off-by: Florian Zavatzki <fz@binsec.com>

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/cvss4/issues
+# Bug-Submit: https://github.com/<user>/cvss4/issues/new
+# Changelog: https://github.com/<user>/cvss4/blob/master/CHANGES
+# Documentation: https://github.com/<user>/cvss4/wiki
+# Repository-Browse: https://github.com/<user>/cvss4
+# Repository: https://github.com/<user>/cvss4.git
